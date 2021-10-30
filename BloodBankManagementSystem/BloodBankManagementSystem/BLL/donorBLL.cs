144e0e464f6d8cacabc81168a20aebc67eec908a
hbkabir004 <hbkabir004@gmail.com>
2021-10-30 22:56:03 +0600

    All in One Commit

---
+++ BloodBankManagementSystem/BloodBankManagementSystem/BLL/donorBLL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodBankManagementSystem.BLL
+{
+    class donorBLL
+    {
+        public int donor_id { get; set; }
+        public string donor_name { get; set; }
+        public string last_donation_date { get; set; }
+        public string contact { get; set; }
+        public string gender { get; set; }
+        public string address { get; set; }
+        public string blood_group { get; set; }
+        public DateTime added_date { get; set; }
+        public string image_name { get; set; }
+        public int added_by { get; set; }
+    }
+}
